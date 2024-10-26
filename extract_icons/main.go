75893f1ef5dd9f480cf25f3842653f0ed85a4494
cesaryuan <cesaryuan@qq.com>
2024-10-26 22:19:06 +0800

    de.buttercookie.disabletargetapiblock

--- extract_icons/main.go
+++ extract_icons/main.go
@@ -40,14 +40,88 @@ func getManifestInfo(manifestPath string) (string, error) {
 	return iconPath, nil
 }
 
+func mergeBackgroundSvgAndForegroundSvg(foregroundSvgPath, backgroundSvgPath string) error {
+	// merge backgroundSvgPath to foregroundSvgPath
+	foregroundSvgContent, err := os.ReadFile(foregroundSvgPath)
+	if err != nil {
+		return err
+	}
+	backgroundSvgContent, err := os.ReadFile(backgroundSvgPath)
+	if err != nil {
+		return err
+	}
+	foregroundMainContentRegex := regexp.MustCompile(`<svg[^>]*>(.*?</svg>)`)
+	foregroundMainContentMatch := foregroundMainContentRegex.FindStringSubmatch(string(foregroundSvgContent))
+	if len(foregroundMainContentMatch) > 1 {
+		backgroundSvgContent = []byte(strings.Replace(string(backgroundSvgContent), `</svg>`, foregroundMainContentMatch[1], 1))
+	}
+	return os.WriteFile(foregroundSvgPath, backgroundSvgContent, 0644)
+}
+
+func convertColorFormat(color string) string {
+	regex := regexp.MustCompile(`#([0-9A-Fa-f]{2})([0-9A-Fa-f]{6})`)
+	if regex.MatchString(color) {
+		return regex.ReplaceAllString(color, "#$2$1")
+	}
+	return color
+}
+
+func extractColorName(colorXmlPath, colorName string) (string, error) {
+	knownColors := map[string]string{
+		"@color/white":                      "#ffffffff",
+		"@color/black":                      "#00000000",
+		"@android:color/white":              "#ffffffff",
+		"@android:color/black":              "#00000000",
+		"@android:color/system_accent2_50":  "#f0f0f0",
+		"@android:color/system_accent2_100": "#dddddd",
+		"@android:color/system_accent2_600": "#999999",
+		"@android:color/system_accent1_600": "#999999",
+	}
+	if color, ok := knownColors[colorName]; ok {
+		return color, nil
+	}
+	if strings.HasPrefix(colorName, "@android:color/") {
+		colorName = strings.TrimPrefix(colorName, "@android:color/")
+		return colorName, nil
+	}
+	if strings.HasPrefix(colorName, "@color/") {
+		colorXmlContent, err := os.ReadFile(colorXmlPath)
+		if err != nil {
+			return "", err
+		}
+		colorBase := filepath.Base(colorName)
+		colorRegex := regexp.MustCompile(fmt.Sprintf(`<color name="%s">([^<]+)</color>`, colorBase))
+		colorMatch := colorRegex.FindStringSubmatch(string(colorXmlContent))
+		if len(colorMatch) > 1 {
+			// format #AARRGGBB to #RRGGBBAA
+			return convertColorFormat(colorMatch[1]), nil
+		}
+	}
+	return "", fmt.Errorf("unknown color: %s", colorName)
+}
+
+func addOrChangeBackground(svgPath, backgroundColor string) error {
+	svgContent, err := os.ReadFile(svgPath)
+	if err != nil {
+		return err
+	}
+	styleRegex := regexp.MustCompile(`(<svg[^>]* style="[^"]*background:)([^ ]+)`)
+	if styleRegex.Match(svgContent) {
+		svgContent = styleRegex.ReplaceAll(svgContent, []byte(fmt.Sprintf(`$1%s`, backgroundColor)))
+	} else {
+		svgContent = []byte(strings.Replace(string(svgContent), "<svg", `<svg style="background:`+backgroundColor+`"`, 1))
+	}
+	return os.WriteFile(svgPath, svgContent, 0644)
+}
+
 func findIconFile(resDir, iconPath string) (string, error) {
 	// get prefix from iconName
+	colorXmlPath := filepath.Join(resDir, "values", "colors.xml")
 	prefix := strings.TrimPrefix(filepath.Dir(iconPath), "@")
 	iconName := filepath.Base(iconPath)
 
 	// find image format first
 	extensions := []string{"webp", "png", "jpeg", "jpg", "svg"}
-
 	for _, ext := range extensions {
 		pattern := filepath.Join(resDir, prefix+"*", iconName+"."+ext)
 		matches, err := filepath.Glob(pattern)
@@ -87,24 +161,72 @@ func findIconFile(resDir, iconPath string) (string, error) {
 			foregroundRegex := regexp.MustCompile(`<foreground android:drawable="([^"]+)"`)
 			foregroundMatch := foregroundRegex.FindStringSubmatch(string(xmlContent))
 			if len(foregroundMatch) > 1 {
-				return findIconFile(resDir, foregroundMatch[1])
+				foregroundSvgPath, err := findIconFile(resDir, foregroundMatch[1])
+				if err != nil {
+					return "", err
+				}
+				if filepath.Ext(foregroundSvgPath) == ".svg" {
+					backgroundRegex := regexp.MustCompile(`<background android:drawable="([^"]+)"`)
+					backgroundMatch := backgroundRegex.FindStringSubmatch(string(xmlContent))
+					if len(backgroundMatch) > 1 {
+						backgroundColor, err := extractColorName(colorXmlPath, backgroundMatch[1])
+						if err == nil {
+							if err := addOrChangeBackground(foregroundSvgPath, backgroundColor); err != nil {
+								fmt.Errorf("Failed to add or change background: %v\n", err)
+							}
+						} else {
+							fmt.Printf("Failed to extract background color name: %v\n", err)
+							backgroundSvgPath, err := findIconFile(resDir, backgroundMatch[1])
+							if err != nil {
+								fmt.Printf("Failed to find background SVG: %v\n", err)
+							} else {
+								if err := mergeBackgroundSvgAndForegroundSvg(foregroundSvgPath, backgroundSvgPath); err != nil {
+									fmt.Printf("Failed to merge background and foreground SVG: %v\n", err)
+								}
+							}
+						}
+					}
+				}
+				return foregroundSvgPath, nil
 			}
 		}
-		// 如果是 <vector>，调用 openai 转换为 svg
+		// 如果是 <vector>，转换为 svg
 		if strings.Contains(string(xmlContent), "<vector") {
-			// 调用 openai 转换为 svg
-			svg, err := converVectorToSvg(string(xmlContent))
+			svgPath := filepath.Join(resDir, iconName+".svg")
+			cmd := exec.Command("npx", "vector-drawable-svg", matches[0], svgPath)
+			if err := cmd.Run(); err != nil {
+				// 调用 openai 转换为 svg
+				svg, err := converVectorToSvg(string(xmlContent))
+				if err != nil {
+					return "", err
+				}
+				// 保存 svg
+
+				svg = strings.TrimSpace(svg)
+				svg = strings.TrimSuffix(svg, "```")
+				svg = strings.TrimLeftFunc(svg, func(r rune) bool {
+					return r != '<'
+				})
+				if err := os.WriteFile(svgPath, []byte(svg), 0644); err != nil {
+					return "", err
+				}
+				return svgPath, nil
+			}
+			// 处理 svg 中形如 @android:color/xxx, @color/xxx 的 color
+			colorRegex := regexp.MustCompile(`@android:color/[^,"]+|@color/[^,"]+`)
+			svgContent, err := os.ReadFile(svgPath)
 			if err != nil {
 				return "", err
 			}
-			// 保存 svg
-			svgPath := filepath.Join(resDir, iconName+".svg")
-			svg = strings.TrimSpace(svg)
-			svg = strings.TrimSuffix(svg, "```")
-			svg = strings.TrimLeftFunc(svg, func(r rune) bool {
-				return r != '<'
+			svgContent = colorRegex.ReplaceAllFunc(svgContent, func(match []byte) []byte {
+				colorName := string(match)
+				color, err := extractColorName(colorXmlPath, colorName)
+				if err != nil {
+					return match
+				}
+				return []byte(color)
 			})
-			if err := os.WriteFile(svgPath, []byte(svg), 0644); err != nil {
+			if err := os.WriteFile(svgPath, svgContent, 0644); err != nil {
 				return "", err
 			}
 			return svgPath, nil
@@ -143,7 +265,8 @@ You are tasked with converting an Android vector drawable XML to an SVG (Scalabl
 a. Remove all Android-specific attributes (those with the "android:" prefix).
 b. Convert the <vector> tag to an <svg> tag.
 c. Set the viewBox attribute of the <svg> tag using the values from android:viewportWidth and android:viewportHeight. The format should be viewBox="0 0 [width] [height]".
-d. Any tag should be closed correctly.
+d. For transform, translate should be placed in the first place.
+e. Any tag should be closed correctly.
 
 d. For each <path> element:
    - Keep the "d" attribute (pathData in Android becomes d in SVG).
@@ -157,7 +280,7 @@ e. If there are any @drawable references (like "@drawable/$ic_launcher_foregroun
 
 2. Output your converted SVG inside <svg> tags. Make sure to include the XML declaration and the SVG namespace.
 3. If all elements's color are white, this means we should add a background color to the svg. For example color: #2B7CD3
-3. Do not output any other redundant things, such as code block identifiers. And don't add anyexplanations, comments, and so on.
+4. Do not output any other redundant things, such as code block identifiers. And don't add anyexplanations, comments, and so on.
 
 Remember, the goal is to create a valid SVG that closely resembles the original Android vector drawable. Some complex features might not have direct SVG equivalents, so use your best judgment to approximate them.
     `
@@ -285,6 +408,10 @@ func main() {
 				continue
 			}
 		}
+		// pkgName := appMap["packageName"].(string)
+		// if pkgName != "de.buttercookie.disabletargetapiblock" {
+		// 	continue
+		// }
 
 		wg.Add(1)
 		semaphore <- struct{}{}
